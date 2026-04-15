7954db926fd5b5a59006b2330831bf0759db44dd
Sandeep Mutnal <sandeepmutnal242@gmail.com>
2026-04-15 19:56:49 +0530

    Visualized dataset relationships using matplotlib

--- python/practice_project/student_marks_predictor/student_marks_predictor.py
+++ python/practice_project/student_marks_predictor/student_marks_predictor.py
@@ -2,7 +2,10 @@
 # Student Marks Predictor
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
+
+# 1️⃣ Dataset
 
 data = {
     "Study_Hours": [2,4,6,8,10,3,5,7,9],
@@ -11,7 +14,32 @@ data = {
     "Marks": [40,50,65,80,95,45,60,75,88]
 }
 
-
 df = pd.DataFrame(data)
 
 print(df)
+
+
+# 2️⃣ Visualization 1: Study Hours vs Marks
+
+plt.plot(df["Study_Hours"], df["Marks"], marker="o")
+
+plt.xlabel("Study Hours")
+plt.ylabel("Marks")
+plt.title("Study Hours vs Marks")
+
+plt.grid(True)
+
+plt.show()
+
+
+# 3️⃣ Visualization 2: Attendance vs Marks
+
+plt.plot(df["Attendance"], df["Marks"], marker="o")
+
+plt.xlabel("Attendance")
+plt.ylabel("Marks")
+plt.title("Attendance vs Marks")
+
+plt.grid(True)
+
+plt.show()
